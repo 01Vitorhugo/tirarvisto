cd60ec803f950d002b58e8eed4eef68141bc885a
Vitor Hugo <vitorhugo0031@outlook.com>
2025-06-05 01:12:33 -0300

    adjusting logic component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,10 @@
 import seta from '../assets/seta.png';
 
-export default function Button({label  }) {
+export default function Button({ label, showImage = true }) {
   return (
-    <button className="bg-button h-10 w-auto flex items-center  rounded-full">
-        {label && <p className='text-textWhite pl-4'>{label}</p>}
-        <img src={seta} className='w-10'/>
-     
+    <button className="bg-button h-10 w-auto flex items-center rounded-full">
+      {label && <p className="text-textWhite p-4">{label}</p>}
+      {showImage && <img src={seta} className="w-10" />}
     </button>
   );
 }

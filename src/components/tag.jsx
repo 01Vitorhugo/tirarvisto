e0ec8a061a17602a9bf2f266c2ede493ce9f87a6
Vitor Hugo <vitorhugo0031@outlook.com>
2025-06-05 01:13:12 -0300

    remove bg

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -2,7 +2,7 @@
 
 export default function Tag({ label }) {
     return (
-        <div className="bg-tag p-5 rounded-full h-auto items-center justify-center inline-flex ">
+        <div className="bg-tag p-5 rounded-full h-[41px] items-center justify-center inline-flex ">
             <p className="font-semibold text-Text">{label}</p>
         </div>
     );

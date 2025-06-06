58f1fec4dc40a865a18f9c3ce851407263121096
Vitor Hugo <vitorhugo0031@outlook.com>
2025-06-06 15:56:07 -0300

    n/a

--- src/components/boxTextHome.jsx
+++ src/components/boxTextHome.jsx
@@ -7,9 +7,11 @@ export default function BoxTextHome() {
         <article className="w-full sm:h-86 bg-boxText pl-15 max-[1018px]:flex max-[1018px]:flex-col  
          max-[1018px]:pr-5  max-[1018px]:pl-5 max-[1018px]:py-5
         md:h-100">
-            <section className="w-[46%] h-3/5  flex items-center max-[1018px]:w-[70%] md:w-[60%] ">
+            <section className="w-[46%] h-3/5  flex items-center max-[1018px]:w-[70%] md:w-[60%] 
+            ">
             <h1 className="text-[30px] md:text-[36px] xl:text-[56px]">CONSULTORIA COMPLETA PARA TIRAR SEU VISTO</h1>
             </section>
+
 
             <section className="w-[100%] h-2/5  flex justify-end items-center   ">
             <article className="w-[65%] h-full  max-[1018px]:w-[70%]  max-[1018px]:pl-10 md:text-center">

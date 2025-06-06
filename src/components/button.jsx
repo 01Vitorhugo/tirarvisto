ed2fb57aeccc3410b9a10d3069471a34b7e75f2d
Vitor Hugo <vitorhugo0031@outlook.com>
2025-06-06 06:23:38 -0300

    add sgv to button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,27 @@
-import seta from '../assets/seta.png';
-
 export default function Button({ label, showImage = true }) {
   return (
-    <button className="bg-button h-10 w-auto flex items-center rounded-full">
-      {label && <p className="text-textWhite p-4">{label}</p>}
-      {showImage && <img src={seta} className="w-10" />}
+    <button className="bg-button h-14 w-auto flex items-center rounded-full py-[10px] pl-[20px] pr-[20px] gap-2 cursor-pointer 
+      hover:bg-transparent border border-button text-white
+      hover:text-button"
+    >
+      {label && <p className=" pl-2 pr-2 font-semibold">{label}</p>}
+      {showImage && (
+        <svg
+          className="w-6 h-6 transition-colors duration-200 hover:stroke-hoverSvgColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          transform="rotate(180)"
+        >
+          <path
+            d="M20 12H4M4 12L10 6M4 12L10 18"
+            className="stroke-current"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      )}
     </button>
   );
 }

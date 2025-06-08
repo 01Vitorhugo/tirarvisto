7bebcb14dc24685b4ae5a03e615720748f2eb436
Vitor Hugo <vitorhugo0031@outlook.com>
2025-06-08 19:25:24 -0300

    add image

--- src/components/boxTextHome.jsx
+++ src/components/boxTextHome.jsx
@@ -1,30 +1,42 @@
-
+import phone from "../assets/phone.webp"
+import time from "../assets/time.webp"
 
 export default function BoxTextHome() {
 
-    return(
+    return (
         <>
-        <article className="w-full sm:h-86 bg-boxText pl-15 max-[1018px]:flex max-[1018px]:flex-col  
+            <article className="w-full sm:h-86 bg-boxText pl-10 pr-10 max-[1018px]:flex max-[1018px]:flex-col  
          max-[1018px]:pr-5  max-[1018px]:pl-5 max-[1018px]:py-5
         md:h-100">
-            <section className="w-[46%] h-3/5  flex items-center max-[1018px]:w-[70%] md:w-[60%] 
-            ">
-            <h1 className="text-[30px] md:text-[36px] xl:text-[56px]">CONSULTORIA COMPLETA PARA TIRAR SEU VISTO</h1>
-            </section>
+                <section className="w-full h-[50%]  flex items-center">
+                    <div className=" w-[70%]">
+                        <h1 className="text-[30px] md:text-[36px] xl:text-[56px]">CONSULTORIA COMPLETA PARA TIRAR SEU VISTO</h1>
+                    </div>
+
+                    <figure className=" w-[30%] flex justify-center">
+                        <img src={phone} alt="" />
+                    </figure>
+                </section>
 
 
-            <section className="w-[100%] h-2/5  flex justify-end items-center   ">
-            <article className="w-[65%] h-full  max-[1018px]:w-[70%]  max-[1018px]:pl-10 md:text-center">
-                 <h1 className=" italic text-[30px] md:text-[36px] xl:text-[56px]">VOCÊ ESTA EM BOAS MÃOS!</h1>
+                <section className="w-[100%]  h-[50%]  flex justify-end items-center   ">
+                    <article className="w-full h-full  flex items-center  max-[1018px]:pl-10 md:text-center">
+                        <figure className=" w-[30%] flex justify-center">
+                            <img src={time} alt="" />
+                        </figure>
+
+                        <div className=" w-[70%]">
+                            <h1 className=" italic text-[30px] md:text-[36px] xl:text-[56px]">VOCÊ ESTA EM BOAS MÃOS!</h1>
+                        </div>
+
+                    </article>
+
+
+                </section>
+
 
             </article>
-       
 
-            </section>
-            
-
-        </article>
-        
         </>
     )
 }
